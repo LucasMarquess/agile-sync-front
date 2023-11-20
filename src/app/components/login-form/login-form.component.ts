1e7aa1bcaf50c3593756f1af1e0b3e61d2389d90
Lucas Marques <lucas.fonseca@itixti.com.br>
2023-11-20 16:20:17 -0300

    feat: adicionando novas validações no formulário de login

--- src/app/components/login-form/login-form.component.ts
+++ src/app/components/login-form/login-form.component.ts
@@ -1,28 +1,42 @@
-import { Component, ElementRef } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, ElementRef, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  FormsModule,
+  ValidatorFn,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'login-form',
   templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.scss']
+  styleUrls: ['./login-form.component.scss'],
 })
-export class LoginFormComponent {
+export class LoginFormComponent implements OnInit {
   hidePassword: boolean = true;
   hidePasswordConfirm: boolean = true;
   isLogin: boolean = true;
   login: string = '';
   password: string = '';
 
+  formulario = new FormGroup(
+    {
+      email: new FormControl('', [Validators.required, Validators.email]),
+      usuario: new FormControl('', [Validators.required]),
+      senha: new FormControl('', [Validators.required]),
+      confirmacaoSenha: new FormControl('', [Validators.required]),
+    },
+    { validators: this.passwordMatchValidator('senha', 'confirmacaoSenha') }
+  );
 
-  formControl = new FormGroup({
-    emailValidator: new FormControl('', [Validators.required, Validators.email])
-  })
+  constructor(private elementRef: ElementRef) {}
 
-  constructor(private elementRef: ElementRef) { }
+  ngOnInit() {}
 
   ngAfterViewInit() {
-    this.elementRef.nativeElement.ownerDocument
-      .body.style.background = 'linear-gradient(to bottom, #024a92, #032b7c)';
+    this.elementRef.nativeElement.ownerDocument.body.style.background =
+      'linear-gradient(to bottom, #024a92, #032b7c)';
   }
 
   changeIsLogin() {
@@ -30,12 +44,55 @@ export class LoginFormComponent {
   }
 
   getErrorMessageEmail() {
-    let control = this.formControl.controls.emailValidator;
-
+    let control = this.formulario.controls.email;
     if (control.hasError('required')) {
-      return 'Você precisa de adicionar um email';
+      return 'Você precisa de adicionar um email.';
     }
+    return control.hasError('email') ? 'Email inválido.' : '';
+  }
 
-    return control.hasError('email') ? 'Email inválido' : '';
+  getErrorMessageUsuario() {
+    let control = this.formulario.controls.usuario;
+    if (control.hasError('required')) {
+      return 'Você precisa de adicionar um usuário.';
+    }
+    return;
+  }
+
+  getErrorMessageSenha() {
+    let control = this.formulario.controls.senha;
+    if (control.hasError('required')) {
+      return 'Você precisa de adicionar uma senha.';
+    }
+    return;
+  }
+
+  getErrorConfirmacaoSenha() {
+    let control = this.formulario.controls.confirmacaoSenha;
+    if (this.formulario.controls.senha.hasError('passwordMismatch')) {
+      return 'As senhas não coincidem.';
+    } else if (
+      control.hasError('required') &&
+      this.formulario.controls.senha.valid
+    ) {
+      return 'Você precisa confirmar a senha escolhida.';
+    }
+    return;
+  }
+
+  passwordMatchValidator(
+    controlName: string,
+    matchingControlName: string
+  ): ValidatorFn {
+    return (control: AbstractControl): { [key: string]: any } | null => {
+      const password = control.get(controlName)?.value;
+      const confirmPassword = control.get(matchingControlName)?.value;
+
+      if (password !== confirmPassword) {
+        return { passwordMismatch: true };
+      }
+
+      return null;
+    };
   }
 }
